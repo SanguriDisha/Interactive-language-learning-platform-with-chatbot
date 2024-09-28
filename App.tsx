81884474bed79bdc08bb8cb8318cd1272dde486f
Disha VijayKumar Sanguri <93032078+SanguriDisha@users.noreply.github.com>
2024-09-28 23:27:12 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import EnhancedLearningPlatform from './EnhancedLearningPlatform';
+
+const App = () => {
+  return <EnhancedLearningPlatform />;
+};
+
+export default App;
